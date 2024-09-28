ddd1dfd7ee1c430bfedcc24c3648d4234257a69d
Sergey Kapustin <kapustin.sergey@gmail.com>
2024-09-28 21:57:26 +0500

    New dev.sh

--- dev.sh
+++ dev.sh
@@ -1,1 +1,10 @@
-cd backend && npm run dev & cd ../frontend && REACT_APP_API_URL=http://127.0.0.1:5001/api npm start
+#!/bin/bash
+
+if [ "$1" = "frontend" ]; then
+    cd frontend && REACT_APP_API_URL=http://127.0.0.1:5001/api npm start
+elif [ "$1" = "backend" ]; then
+    cd backend && npm run dev
+else
+    echo "Usage: ./dev.sh [frontend|backend]"
+    exit 1
+fi
